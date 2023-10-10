4e65c6866e2e46a3e1d13eefc6cd36451e09560b
BernardGrand <99039053+BernardGrand@users.noreply.github.com>
2023-10-10 12:02:43 +0200

    final v2
    made small changes
    added Mici's button

---
+++ style/etc/fogyi.js
@@ -0,0 +1,12 @@
+// Get references to the elements
+const navicon1 = document.getElementById("navicon1base");
+const navicon2 = document.getElementById("navicon2base");
+const navicon3 = document.getElementById("navicon3base");
+const changeButton = document.getElementById("AGomb");
+
+// Function to change the background color to black
+function changeBackgroundColor() {
+  navicon1.style.backgroundColor = "black";
+  navicon2.style.backgroundColor = "black";
+  navicon3.style.backgroundColor = "black";
+}
